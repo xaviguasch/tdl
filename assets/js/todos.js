60be88f455bc57974853f4c147b57c2f83c8a97c
xaviguasch <xaviguasch@gmail.com>
2018-12-18 18:56:46 +0100

    creating new todos and adding functionality to them

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,14 +1,25 @@
 // Check off specific Todos by clicking
-$('li').click(function() {
+$('ul').on('click', 'li', function() {
     $(this).toggleClass('completed')
 })
 
 
 // Click on X to delete Todos
 
-$('span').click(function(event) {
+$('ul').on('click', 'span', function(event) {
     event.stopPropagation()
     $(this).parent().fadeOut(500, function() {
         $(this).remove()
     })
+})
+
+$('input[type="text"]').keypress(function(event) {
+        if (event.which === 13) { // we're checking for the enter key
+            // grabbing new todo text from input
+            const todoText = $(this).val()
+            $(this).val("")
+            
+            // create a new li and add to ul
+            $('ul').append(`<li><span>X</span> ${todoText}</li>`)
+        }
 })
